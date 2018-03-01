faa23efc710b8f857633c5fae099d54a5c339cbd
adam n <adam.nawroc@gmail.com>
2018-03-01 12:36:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,68 @@
 
+def sort_array_asc(arr)
+  arr.sort
+
+end
+
+def sort_array_desc(arr)
+  arr.sort.reverse
+
+end
+
+def sort_array_char_count(arr)
+  arr.sort{|x, y| x.length <=> y.length}
+
+end
+
+def swap_elements(arr)
+    arr[-2], arr[-1] = arr[-1], arr[-2]
+    arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+
+end
+
+def kesha_maker(arr)
+
+  arr.collect do |i|
+  	# just return the 2th element
+    i[2] = '$'
+    i
+  end
+
+end
+
+def sum_array(arr)
+
+  i = 0
+
+  arr.each do |n|
+    i += n
+  end
+
+  i
+
+end
+
+def add_s(arr)
+  x = 0
+  arr.collect do |i|
+    x+=1
+
+    if x == 2
+      i
+    else
+      i + 's'
+    end
+
+  end
+
+end
+
+def find_a(arr)
+
+  arr.select {|w| w[0] == 'a'}
+
+end
